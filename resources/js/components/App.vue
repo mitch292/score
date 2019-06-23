ee6bec4b1295ead8c6b9fad506d55ca0b621bb35
Andrew Mitchell <andrewpmitchell7@gmail.com>
2019-06-23 14:06:01 -0400

    Project creation

---
+++ resources/js/components/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <span>
+
+    <div class="header">
+      <span class="header__icon">
+        <font-awesome-icon icon="baseball-ball" size="3x" />
+        <span class="header__text">score</span>
+      </span>
+    </div>
+
+    <div class="content">
+      <card>
+        helloooo
+      </card>
+      <card></card>
+      <card></card>
+    </div>
+
+    <div class="footer">
+
+    </div>
+
+  </span>
+</template>
+
+<script>
+import Card from './Card.vue'
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+        components: { Card }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/components/Card.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card">
+    <div class="card__content">
+      <slot></slot>
+    </div>
+    <div class="card__stripe"></div>
+    <div class="card__bottom"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    mounted() {
+      console.log('hello, card');
+    }
+  }
+</script>
+
+<style>
+
+</style>
