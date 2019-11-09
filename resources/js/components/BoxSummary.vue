43a4be0455262425b69c0a82da9a7b3fed067961
Andrew Mitchell <andrewpmitchell7@gmail.com>
2019-11-09 16:13:56 -0500

    Handle different game states

--- resources/js/components/BoxSummary.vue
+++ resources/js/components/BoxSummary.vue
@@ -1,44 +1,47 @@
 <template>
-    <div class="mt-4 row">
+    <div class="mt-4">
+        <div class="row justify-content-center">
+            <div class="btn btn-outline-primary">{{gameState}}</div>
+        </div>
+        <div class="row mt-4">
+            <table class="score-table d-block mx-auto">
 
-        <table class="score-table d-block mx-auto">
+                <tr>
+                    <th>team</th>
+                    <th v-for="n in getNumberOfInnings(gameData)" :key="n" :class="{'text-red': currentInning === n && gameState !== GAME_IS_FINAL}">
+                        {{ n }}
+                    </th>
+                    <th class="text-red">R</th>
+                    <th>H</th>
+                    <th>E</th>
+                    <th>LoB</th>
+                </tr>
 
-            <tr>
-                <th>team</th>
-                <th v-for="n in getNumberOfInnings(gameData)" :key="n" :class="{'text-red': currentInning === n && gameState !== GAME_IS_FINAL}">
-                    {{ n }}
-                </th>
-                <th :class="{'text-red':gameState !== GAME_IS_FINAL}">R</th>
-                <th>H</th>
-                <th>E</th>
-                <th>LoB</th>
-            </tr>
+                <tr>
+                    <td>{{ awayTeam }}</td>
+                    <td v-for="n in getNumberOfInnings(gameData)" :key="n" :class="{'text-red': currentInning === n && gameData.linescore.isTopInning && gameState !== GAME_IS_FINAL}">
+                        {{ getRuns('away', n) }}
+                    </td>
+                    <td class="text-red">{{ gameData.linescore.teams.away.runs }}</td>
+                    <td>{{ gameData.linescore.teams.away.hits }}</td>
+                    <td>{{ gameData.linescore.teams.away.errors }}</td>
+                    <td>{{ gameData.linescore.teams.away.leftOnBase }}</td>
+                </tr>
+                <tr>
+                    <td>{{ homeTeam }}</td>
+                    <td v-for="n in getNumberOfInnings(gameData)" :key="n" :class="{'text-red': currentInning === n && !gameData.linescore.isTopInning && gameState !== GAME_IS_FINAL}">
+                        {{ getRuns('home', n) }}
+                    </td>
+                    <td class="text-red">{{ gameData.linescore.teams.home.runs }}</td>
+                    <td>{{ gameData.linescore.teams.home.hits }}</td>
+                    <td>{{ gameData.linescore.teams.home.errors }}</td>
+                    <td>{{ gameData.linescore.teams.home.leftOnBase }}</td>
+                </tr>
 
-            <tr>
-                <td>{{ awayTeam }}</td>
-                <td v-for="n in getNumberOfInnings(gameData)" :key="n" :class="{'text-red': currentInning === n && gameData.linescore.isTopInning && gameState !== GAME_IS_FINAL}">
-                    {{ getRuns('away', n) }}
-                </td>
-                <td class="text-red">{{ gameData.linescore.teams.away.runs }}</td>
-                <td>{{ gameData.linescore.teams.away.hits }}</td>
-                <td>{{ gameData.linescore.teams.away.errors }}</td>
-                <td>{{ gameData.linescore.teams.away.leftOnBase }}</td>
-            </tr>
-            <tr>
-                <td>{{ homeTeam }}</td>
-                <td v-for="n in getNumberOfInnings(gameData)" :key="n" :class="{'text-red': currentInning === n && !gameData.linescore.isTopInning && gameState !== GAME_IS_FINAL}">
-                    {{ getRuns('home', n) }}
-                </td>
-                <td class="text-red">{{ gameData.linescore.teams.home.runs }}</td>
-                <td>{{ gameData.linescore.teams.home.hits }}</td>
-                <td>{{ gameData.linescore.teams.home.errors }}</td>
-                <td>{{ gameData.linescore.teams.home.leftOnBase }}</td>
-            </tr>
+            </table>
 
-        </table>
-
+        </div>
     </div>
-
 </template>
 
 <script>
