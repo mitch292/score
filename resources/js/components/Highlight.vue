97c642e2a64e875e06e15c9257302e55f3cf7fce
Andrew Mitchell <andrewpmitchell7@gmail.com>
2019-12-01 08:41:26 -0500

    Add a sub highlight component

---
+++ resources/js/components/Highlight.vue
@@ -0,0 +1,44 @@
+<template>
+
+	<utility-card>
+		
+		<template slot="content">
+			<video width="100%" height="100%" controls>
+				<source :src="highlight.playback_url" type="video/mp4">
+			</video>
+		</template>
+		
+		<template slot="bottom">
+			<div class="text-center mt-4">
+				<div class="h5 text-red">{{title}}</div>
+				<div class="m-2 p-2">{{description}}</div>
+			</div>
+		</template>
+
+	</utility-card>
+
+</template>
+
+<script>
+	import UtilityCard from './utility/Card.vue';
+
+	export default {
+		props: {
+			highlight: Object
+		},
+		computed: {
+			title() {
+				return this.highlight.title.toLowerCase();
+			},
+			description() {
+				return this.highlight.description.toLowerCase();
+			}
+		},
+		components: { UtilityCard }
+
+	}
+</script>
+
+<style>
+
+</style>
--- resources/js/components/Highlights.vue
+++ resources/js/components/Highlights.vue
@@ -4,26 +4,17 @@
 	</div>
 	
 	<div v-else>
-		<utility-card v-for="highlight in formattedHighlights" :key="highlight.id">
-			<template slot="content">
-				<video width="100%" height="100%" controls>
-					<source :src="highlight.playback_url" type="video/mp4">
-				</video>
-			</template>
-			<template slot="bottom">
-				<div class="text-center mt-4">
-					<div class="h5 text-red">{{highlight.title}}</div>
-					<div class="m-2 p-2">{{highlight.description}}</div>
-				</div>
-			</template>
-		</utility-card>
-
+		<div v-for="highlight in highlights" :key="highlight.id">
+			<score-highlight :highlight="highlight"></score-highlight>
+		</div>
 	</div>
+	
 </template>
 
 <script>
 	import UtilityLoading from './utility/Loading.vue';
-	import UtilityCard from './utility/Card.vue';
+	import ScoreHighlight from './Highlight.vue';
+
 	export default {
 		props: {
 			gamePk: String
@@ -60,7 +51,7 @@
 		mounted() {
 			this.getHighlights(this.gamePk);
 		},
-		components: { UtilityLoading, UtilityCard }
+		components: { UtilityLoading, ScoreHighlight }
 
 	}
 </script>
