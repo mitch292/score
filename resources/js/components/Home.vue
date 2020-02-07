1ffa1a54bc13ed524ebe79916ca7417e01931560
Andrew Mitchell <andrewpmitchell7@gmail.com>
2020-02-06 20:11:29 -0500

    Assets and basic layout for homepage

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,5 +1,32 @@
 <template>
-  <div>Home</div>
+	<div>
+		<div class="row mt-5">
+			<div class="col-4">
+				<img class="homepage-image" alt="confused-human" src="/images/homepage/cherry-delivery.png">
+
+			</div>
+			<div class="col-8 h3 text-red align-text-bottom">ever forget the details of your favorite baseball games?</div>
+		</div>
+		<div class="row mt-5 justify-content-end">
+			<div class="col-8 h3 text-red align-text-bottom">that's okay, stop the frustration.</div>
+			<div class="col-4">
+				<img class="homepage-image" alt="frustrated-human" src="/images/homepage/cherry-fatal-error.png">
+			</div>
+		</div>
+		<div class="row mt-5">
+			<div class="col-4">
+				<img class="homepage-image" alt="strong-human" src="/images/homepage/cherry-upgrade.png">
+			</div>
+			<div class="col-8 h3 text-red align-text-bottom">find your game, check out the details, browse others.</div>
+		</div>
+		<div class="row justify-content-end">
+			<div class="col-8 h3 text-red">save highlights, save games, cherish memories</div>
+			<div class="col-4">
+				<img class="homepage-image" alt="happy-human" src="/images/homepage/cherry-success.png">
+			</div>
+		</div>
+
+	</div>
 </template>
 
 <script>
@@ -9,5 +36,7 @@ export default {
 </script>
 
 <style>
-
+	.homepage-image {
+		height: 250px;
+	}
 </style>
