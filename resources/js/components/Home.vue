6e2c570a5fd0ba4a93fc43a363f93ce6b84d5f5d
Andrew Mitchell <andrewpmitchell7@gmail.com>
2020-02-06 20:19:57 -0500

    MVP for homepage in desktop and mobile view

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,27 +1,30 @@
 <template>
-	<div>
-		<div class="row mt-5">
-			<div class="col-4">
-				<img class="homepage-image" alt="confused-human" src="/images/homepage/cherry-delivery.png">
+	<div class="text-center p-1">
 
+		<div class="row mt-5">
+			<div class="col-12 col-sm-4">
+				<img class="homepage-image" alt="confused-human" src="/images/homepage/cherry-delivery.png">
 			</div>
-			<div class="col-8 h3 text-red align-text-bottom">ever forget the details of your favorite baseball games?</div>
+			<div class="col-12 col-sm-8 h3 text-red mt-5">ever forget the details of your favorite baseball games?</div>
 		</div>
+
 		<div class="row mt-5 justify-content-end">
-			<div class="col-8 h3 text-red align-text-bottom">that's okay, stop the frustration.</div>
-			<div class="col-4">
+			<div class="col-12 col-sm-8 h3 text-red mt-5">that's okay, stop the frustration.</div>
+			<div class="col-12 col-sm-4">
 				<img class="homepage-image" alt="frustrated-human" src="/images/homepage/cherry-fatal-error.png">
 			</div>
 		</div>
+
 		<div class="row mt-5">
-			<div class="col-4">
+			<div class="col-12 col-sm-4">
 				<img class="homepage-image" alt="strong-human" src="/images/homepage/cherry-upgrade.png">
 			</div>
-			<div class="col-8 h3 text-red align-text-bottom">find your game, check out the details, browse others.</div>
+			<div class="col-sm-8 h3 text-red mt-5">find your game, check out the details, browse others.</div>
 		</div>
-		<div class="row justify-content-end">
-			<div class="col-8 h3 text-red">save highlights, save games, cherish memories</div>
-			<div class="col-4">
+
+		<div class="row mt-5 justify-content-end">
+			<div class="col-12 col-sm-8 h3 text-red mt-5">save highlights, save games, cherish memories</div>
+			<div class="col-12 col-sm-4">
 				<img class="homepage-image" alt="happy-human" src="/images/homepage/cherry-success.png">
 			</div>
 		</div>
